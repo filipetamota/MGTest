a3e54ff05341a5a405080163114c1902a26d5a2c
Filipe <Filipe@dhcp-193-136-238-122.ci.uc.pt>
2013-02-20 16:37:56 +0000

    Initial Commit

---
+++ MGTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MGTest
+//
+//  Created by Filipe on 20/02/13.
+//  Copyright (c) 2013 Filipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
